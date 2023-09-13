2a867e9432b79af826bd8b9850cddca79d598f17
Noemi souza <noemigiseleee@gmail.com>
2023-09-13 19:24:05 -0300

    fix lint 1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,82 +1,81 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('testes do requisito 1', () => {
-  renderWithRouter(<App />)
-  const linkHome = screen.getByText(/home/i)
-  const linkAbout = screen.getByText(/about/i)
-  const linkFavorite = screen.getByText(/favorite pokémon/i)
-
   test('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-    expect(linkHome).toBeInTheDocument()
-    expect(linkAbout).toBeInTheDocument()
-    expect(linkFavorite).toBeInTheDocument()
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText(/home/i);
+    const linkAbout = screen.getByText(/about/i);
+    const linkFavorite = screen.getByText(/favorite pokémon/i);
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
   });
 
-  test(`Teste se a aplicação é redirecionada para a página inicial, na URL /, ao clicar no link Home da barra de navegação`, () => {
-    renderWithRouter(<App />)
+  test('Teste se a aplicação é redirecionada para a página inicial'
+    + ', na URL /, ao clicar no link Home da barra de navegação', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByText(/home/i);
     fireEvent.click(linkHome);
 
     const home = screen.getByRole('heading', {
-      name: /encountered pokémon/i
-    })
+      name: /encountered pokémon/i,
+    });
     const button = screen.getByRole('button', {
-      name: /próximo pokémon/i
-    })
+      name: /próximo pokémon/i,
+    });
 
-    expect(home).toBeInTheDocument()
-    expect(button).toBeInTheDocument()
+    expect(home).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 
   test('se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação', () => {
-    renderWithRouter(<App />)
-    const linkAbout = screen.getByText(/about/i)
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByText(/about/i);
 
-    fireEvent.click(linkAbout)
+    fireEvent.click(linkAbout);
 
     const aboutPokedex = screen.getByRole('heading', {
-      name: /about pokédex/i
-    })
+      name: /about pokédex/i,
+    });
     const headingPokedex = screen.getByRole('heading', {
-      name: /what does this app do\?/i
-    })
+      name: /what does this app do\?/i,
+    });
 
-    expect(aboutPokedex).toBeInTheDocument()
-    expect(headingPokedex).toBeInTheDocument()
-  })
+    expect(aboutPokedex).toBeInTheDocument();
+    expect(headingPokedex).toBeInTheDocument();
+  });
 
   test('se a aplicação é redirecionada para a página de favorites, na URL /favorites, ao clicar no link favorites da barra de navegação', () => {
-    renderWithRouter(<App />)
-    const linkFavorite = screen.getByText(/favorite pokémon/i)
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByText(/favorite pokémon/i);
 
-    fireEvent.click(linkFavorite)
+    fireEvent.click(linkFavorite);
 
     const headingPokedex = screen.getByRole('heading', {
-      name: /favorite pokémon/i
-    })
-    const text = screen.getByText(/no favorite pokémon found/i)
+      name: /favorite pokémon/i,
+    });
+    const text = screen.getByText(/no favorite pokémon found/i);
 
-    expect(headingPokedex).toBeInTheDocument()
-    expect(text).toBeInTheDocument()
-  })
+    expect(headingPokedex).toBeInTheDocument();
+    expect(text).toBeInTheDocument();
+  });
 
   test('se a aplicação é redirecionada para a página de favorites, na URL /favorites, ao clicar no link favorites da barra de navegação', () => {
-    renderWithRouter(<App />, { route: '/abobora'})
+    renderWithRouter(<App />, { route: '/abobora' });
 
     const notFoundText = screen.getByRole('heading', {
-      name: /page requested not found/i
-    })
+      name: /page requested not found/i,
+    });
 
-    const notFoundImage = screen
-    .getByRole(
-      'img',
-      {  
-        name: /clefairy pushing buttons randomly with text i have no idea what i'm doing/i
-      }
-    ) 
+    const notFoundImage = screen.getByRole('img', {
+      name: /clefairy pushing buttons randomly with text i have no idea what i'm doing/i,
+    });
 
-    expect(notFoundText).toBeInTheDocument()
-    expect(notFoundImage).toBeInTheDocument()
-  })
-})
+    expect(notFoundText).toBeInTheDocument();
+    expect(notFoundImage).toBeInTheDocument();
+  });
+});
